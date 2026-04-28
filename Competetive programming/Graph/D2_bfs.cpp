ddd798cef9d50482e55625cb58790b81c8abe782
Ram Chandra Gupta <ramcgupta.ml@gmail.com>
2026-04-28 11:18:15 +0530

    added bfs graph

---
+++ Competetive programming/Graph/D2_bfs.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+} *head[5];
+
+vector<int> graph[5];
+void addEdge(int u, int v)
+{
+    graph[u].push_back(v);
+}
+
+void bfs(int s)
+{
+    bool vis[5] = {};
+    queue<int> Q;
+    Q.push(s);
+    vis[s] = true;
+    while (!Q.empty())
+    {
+        int curr = Q.front();
+        Q.pop();
+        cout << curr << " ";
+        for (int i = 0; i < graph[curr].size(); i++)
+        {
+            int v = graph[curr][i];
+            if (vis[v] == false)
+            {
+                vis[v] = true;
+                Q.push(v);
+            }
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // Build the graph
+    addEdge(0, 1);
+    addEdge(1, 2);
+    addEdge(1, 3);
+    addEdge(2, 3);
+    addEdge(2, 4);
+
+    // Perform BFS traversal starting from node 0
+    cout << "BFS Traversal: ";
+    bfs(0);
+
+    return 0;
+}
